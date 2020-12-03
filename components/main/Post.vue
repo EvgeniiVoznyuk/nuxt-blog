65bef43db12d22cdf4d34dac4235c24355b1f125
Evgenii <voznukzhenia@gmail.com>
2020-12-03 14:51:09 +0200

    add post page

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,17 +1,67 @@
 <template>
-  <article class="post" />
+  <el-card shadow="hover" :body-style="{ padding: 0 }" class="post">
+    <header slot="header" class="post__header">
+      <h3>Post title</h3>
+      <small>
+        <i class="el-icon-time" />
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post__body">
+      <img
+        src="https://tuhub.ru/sites/default/files/2018-02/vuejs-logo_0.jpg"
+        alt="post-img"
+        class="post__img"
+      >
+      <hr>
+    </div>
+    <footer class="post__footer">
+      <el-button round @click="openPost">
+        Открыть
+      </el-button>
+      <span class="post__comments">
+        <i class="el-icon-message" />
+        12
+      </span>
+    </footer>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    openPost () {
+      const id = 'test-id'
+      this.$router.push(`/post/${id}`)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .post {
-  height: 200px;
-  border: 1px solid black;
-  margin-bottom: 2rem;
+  margin-bottom: 1.5rem;
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__img {
+    width: 100%;
+    height: auto;
+  }
+
+  &__footer {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__comments {
+    cursor: pointer;
+  }
 }
 </style>
---
+++ pages/post/_id.vue
@@ -0,0 +1,65 @@
+<template>
+  <article class="post">
+    <header class="post__header">
+      <div class="post__title">
+        <h1>Post title</h1>
+        <nuxt-link to="/" />
+        <i class="el-icon-back" />
+      </div>
+      <div class="post__info">
+        <small>
+          <i class="el-icon-time" />
+          {{ new Date().toLocaleString() }}
+        </small>
+        <small>
+          42
+          <i class="el-icon-view" />
+        </small>
+      </div>
+      <div class="post__img">
+        <img
+          src="https://tuhub.ru/sites/default/files/2018-02/vuejs-logo_0.jpg"
+          alt="post-img"
+          class="post__img"
+        >
+      </div>
+    </header>
+    <main class="post__main">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas, accusantium?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas, accusantium?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas, accusantium?</p>
+    </main>
+  </article>
+</template>
+
+<script>
+export default {
+  validate ({ params }) {
+    return !!params.id
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.post {
+  max-width: 600px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &__title {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+    align-items: center;
+  }
+
+  &__info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 0.5rem;
+  }
+}
+</style>
